b082e4dec340b99cc82e2a6774e93e0b9bad7c70
unknown <muax000@gmail.com>
2017-12-01 18:19:16 +0500

    modified2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,17 +7,17 @@
     <div class="col-sm-2 sidenav">
     <div class="well">
       <br>
-    <a style= "font-family:DAM_Madheeh; font-size: 1.4em; background-color:rgb(230, 253, 233);color:black;"
+    <a style= "font-family:Mv_Iyyu_Normal; font-size: 1.4em; background-color:rgb(230, 253, 233);color:black;"
     
     
     
     href="#" class="list-group-item active"> ކަށުނަމާދު ކުރާނެ ގޮތް</a>
     <br>
-    <a  style= "font-family:DAM_Madheeh; font-size: 1.2em;background-color:rgb(230, 253, 233);color:black; " href="#" class="list-group-item active" src:('fonts/MVWaheed.otf')>މައްޔިތާ ވަޅުލަމުން ކުރާ ދުޢާ</a>
+    <a  style= "font-family:Mv_Iyyu_Normal; font-size: 1.2em;background-color:rgb(230, 253, 233);color:black; " href="#" class="list-group-item active" src:('fonts/MVWaheed.otf')>މައްޔިތާ ވަޅުލަމުން ކުރާ ދުޢާ</a>
     <br>
-    <a  style= "font-family:DAM_Madheeh; font-size: 1.1em;;background-color:rgb(230, 253, 233);color:black;"  href="#" class="list-group-item active">މައްޔިތައި މޫނު ބަލަން ދިއުމަކީ ކޮންކަހަލަ ކަމެއްތޯ؟</a>
+    <a  style= "font-family:Mv_Iyyu_Normal; font-size: 1.1em;;background-color:rgb(230, 253, 233);color:black;"  href="#" class="list-group-item active">މައްޔިތައި މޫނު ބަލަން ދިއުމަކީ ކޮންކަހަލަ ކަމެއްތޯ؟</a>
     <br>
-    <a  style= "font-family:DAM_Madheeh; font-size: 1.4em;;background-color:rgb(230, 253, 233);color:black;"  href="#" class="list-group-item active" > އޭ އިންސާނާ އެވެ. މުޅި ހަޔާތް ހޭދަކުރަނީ މުދާ ޖަމާކުރުމުގައެވެ. ކަފުނުގައި ޖީބެއް ނުހުންނަކަމައި ކަށްވަޅުގައި ތިޖޫރިއެއް ނުހުންނަކަން ފަހަރެއްގައިވެސް ތިބާ ހަނދާނެއް ނެތެވެ. </a>  
+    <a  style= "font-family:Mv_Iyyu_Normal; font-size: 1.4em;;background-color:rgb(230, 253, 233);color:black;"  href="#" class="list-group-item active" > އޭ އިންސާނާ އެވެ. މުޅި ހަޔާތް ހޭދަކުރަނީ މުދާ ޖަމާކުރުމުގައެވެ. ކަފުނުގައި ޖީބެއް ނުހުންނަކަމައި ކަށްވަޅުގައި ތިޖޫރިއެއް ނުހުންނަކަން ފަހަރެއްގައިވެސް ތިބާ ހަނދާނެއް ނެތެވެ. </a>  
     <br>
    
 
